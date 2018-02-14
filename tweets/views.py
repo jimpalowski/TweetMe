ec8a49ce41227100aabf8b8f74791421f6551e80
Jim Palowski <palowskijim@gmail.com>
2018-02-13 21:05:47 -0800

    Updating

---
+++ tweets/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+from tweets.models import Tweet
+# Create your views here.
+
+#Create
+
+#Retrieve
+
+#Update
+
+#Delete
+
+#List / Search
+
+#Retrieve
+def tweet_detail_view(request, id=1):
+	obj = Tweet.objects.get(id=id)
+	print(obj)
+	context = {
+		"object": obj
+	}
+	return render(request, "tweets/detail_view.html", context)
+
+def tweet_list_view(request):
+	queryset = Tweet.objects.all()
+	print(queryset)
+	for obj in queryset:
+		print(obj.content)
+	context = {
+		"object_list": queryset
+	}
+	return render(request, "tweets/list_view.html", context)	
